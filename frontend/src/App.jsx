805c6a4356b6ea90ec6163fb54a481b4350a58c9
cgordon0 <cgordon0@byu.edu>
2026-04-24 10:34:56 -0600

    create borders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import putinTrumpu from './assets/trump_putin_nodding.webm'
 import './App.css'
 
 function App() {
@@ -9,16 +9,25 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <div className="video-background">
+        <video autoPlay loop muted playsInline className="bg-video left">
+          <source src={putinTrumpu} type="video/webm" />
+        </video>
+
+        <video autoPlay loop muted playsInline className="bg-video right">
+          <source src={putinTrumpu} type="video/webm" />
+        </video>
+      </div>
+
+      <section id="center" className="dimmed">
+
+        <div className="myhero">
+          <img src="/trump.gif" className="base" width="170" height="179" alt="trumpu" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Welcome to Stock MLM</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Where all your dreams come true
           </p>
         </div>
         <button
